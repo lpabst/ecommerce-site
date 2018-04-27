5e80947cebf98c0adf3b01744c6d523fb469deeb
lorenpabst@gmail.com <lorenpabst@gmail.com>
2018-04-27 14:41:45 -0600

    simplified code on admin page a bit

--- src/routes/Admin/AdminLanding.js
+++ src/routes/Admin/AdminLanding.js
@@ -22,11 +22,6 @@ class AdminLanding extends Component {
       loadingPage: true,
     }
     
-    this.handleImageInput = this.handleImageInput.bind(this);
-    this.handleTitleInput = this.handleTitleInput.bind(this);
-    this.handleDescriptionInput = this.handleDescriptionInput.bind(this);
-    this.handlePriceInput = this.handlePriceInput.bind(this);
-    this.handleAttributesInput = this.handleAttributesInput.bind(this);
     this.submit = this.submit.bind(this);
     this.editProduct = this.editProduct.bind(this);
     this.cancelEdit = this.cancelEdit.bind(this);
@@ -51,57 +46,34 @@ class AdminLanding extends Component {
     });
   }
 
-  handleImageInput(e){
-    this.setState({
-      imageInput: e.target.value
-    })
-  }
-
-  handleTitleInput(e){
-    this.setState({
-      titleInput: e.target.value
-    })
-  }
-  handleDescriptionInput(e){
-    this.setState({
-      descriptionInput: e.target.value
-    })
-  }
-  handlePriceInput(e){
-    this.setState({
-      priceInput: e.target.value
-    })
-  }
-  handleAttributesInput(e){
-    this.setState({
-      attributesInput: e.target.value
-    })
-  }
-
   submit() {
     if(this.state.editClicked){
-      axios.patch('/api/updateProduct', { "productID": this.state.productID,
-                                          "title": this.state.titleInput, 
-                                          "description": this.state.descriptionInput, 
-                                          "price": this.state.priceInput, 
-                                          "image": this.state.imageInput, 
-                                          "attributes": this.state.attributesInput})
-          .then(res => {
-            alert(res.data);
-            this.cancelEdit();
-            this.child.componentDidMount()
-          })
+      axios.patch('/api/updateProduct', { 
+        "productID": this.state.productID,
+        "title": this.state.titleInput, 
+        "description": this.state.descriptionInput, 
+        "price": this.state.priceInput, 
+        "image": this.state.imageInput, 
+        "attributes": this.state.attributesInput
+      })
+      .then(res => {
+        alert(res.data);
+        this.cancelEdit();
+        this.child.componentDidMount()
+      })
     } else {
-      axios.post('/api/addProduct', { "title": this.state.titleInput, 
-                                      "description": this.state.descriptionInput, 
-                                      "price": this.state.priceInput, 
-                                      "image": this.state.imageInput, 
-                                      "attributes": this.state.attributesInput})
-          .then(res => {
-            alert(res.data);
-            this.cancelEdit();
-            this.child.componentDidMount()
-          })
+      axios.post('/api/addProduct', { 
+        "title": this.state.titleInput, 
+        "description": this.state.descriptionInput, 
+        "price": this.state.priceInput, 
+        "image": this.state.imageInput, 
+        "attributes": this.state.attributesInput
+      })
+      .then(res => {
+        alert(res.data);
+        this.cancelEdit();
+        this.child.componentDidMount()
+      })
     }
   }
 
@@ -159,19 +131,19 @@ class AdminLanding extends Component {
                 <div className='alAddNewProductSection'>
                   <div className='alInputsWrapper'>
                     <h1>Image Source</h1>
-                    <input value={this.state.imageInput} onChange={this.handleImageInput} type="text"/>
+                    <input value={this.state.imageInput} onChange={(e) => this.setState({imageInput: e.target.value})} type="text"/>
                     <br/>
                     <h1>Title</h1>
-                    <input value={this.state.titleInput} onChange={this.handleTitleInput} type="text"/>
+                    <input value={this.state.titleInput} onChange={(e) => this.setState({titleInput: e.target.value})} type="text"/>
                     <br/>
                     <h1>Description</h1>
-                    <textarea value={this.state.descriptionInput} onChange={this.handleDescriptionInput} type="text"/>
+                    <textarea value={this.state.descriptionInput} onChange={(e) => this.setState({descriptionInput: e.target.value})} type="text"/>
                     <br/>
                     <h1>Price</h1>
-                    <input value={this.state.priceInput} onChange={this.handlePriceInput} type="text"/>
+                    <input value={this.state.priceInput} onChange={(e) => this.setState({priceInput: e.target.value})} type="text"/>
                     <br/>
                     <h1>Attributes (Attributes should be capitalized and separated by a space only.)</h1>
-                    <input value={this.state.attributesInput} onChange={this.handleAttributesInput} type="text"/>
+                    <input value={this.state.attributesInput} onChange={(e) => this.setState({attributesInput: e.target.value})} type="text"/>
                     <br/>
                     <button onClick={this.submit}>Submit</button>
                     {cancelEdit}
