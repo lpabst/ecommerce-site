84a990b933155a1c3c835026a6d680ada4d4453c
lorenpabst@gmail.com <lorenpabst@gmail.com>
2018-04-28 13:33:25 -0600

    mobile optimizing the products

--- src/routes/UnderConstruction/UnderConstruction.jsx
+++ src/routes/UnderConstruction/UnderConstruction.jsx
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import './UnderConstruction.css'
 
 import MainHeader from './../../components/Headers/MainHeader.js';
+import MainFooter from './../../components/Footers/MainFooter.js';
 
 class UnderConstruction extends Component {
     render() {
         return (
             <section className='routeWrapper'>
             
-                <MainHeader/>
+                <MainHeader />
 
                 <div className='constructionContent'>
                     <p className='constructionWords' >This page is currently under construction. Thank you for your patience!</p>
                     <img className='fullScreenImg' src="https://images.pexels.com/photos/159306/construction-site-build-construction-work-159306.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="construction worker"/>
                 </div>
+
+                <MainFooter />
                 
             </section>
         );
